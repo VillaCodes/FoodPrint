7db89f4b2b206fe339fb06aefeac86eef6afb0f9
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2022-03-13 19:46:21 -0700

    feat(server): installed express server

--- src/components/IngredientList/IngredientsContainer.tsx
+++ src/components/IngredientList/IngredientsContainer.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import Ingredients from "./Ingredients";
 import "./IngredientList.css"
+import { FoodprintContext } from "../../store/ingredient-context";
 
 export default function IngredientsContainer () {
-   
+   const [userIngredient, setUserIngredient] = useState<string>("");
+   const {addIngredient, items} = useContext(FoodprintContext).ingredients;
+
+   const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+       event.preventDefault();
+       const formattedIngredient = userIngredient.toLowerCase();
+       items.map(e => e.text).includes(formattedIngredient) ? null : addIngredient(formattedIngredient);
+       setUserIngredient("")
+   }
     
     return (
         <ul className="Ingredients-Container">
             <header>Current Ingredients</header>
             <Ingredients />
+            <form onSubmit={submitHandler}>
+            <input
+            id="userIngredient"
+            placeholder="Add an ingredient"
+            type ="text"
+            onChange={(event) => setUserIngredient(event?.target.value)}
+            value={userIngredient}
+            />
+            </form>
         </ul>
     )
 }
