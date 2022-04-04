837cf04eb25f4963e2a8a098fb73e7614cba155f
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2022-04-04 15:14:19 -0700

    feat(router): React router v6 added to project. Router is wrapping App in main.tsx. Recipe read request helper created. Styling has begun on cards. RecipeInfo interface created for typing. RecipePage.tsx is being initialized after this commit.

--- src/utils/SpoonacularRequests.ts
+++ src/utils/SpoonacularRequests.ts
@@ -1,6 +1,14 @@
 import { onCallHandler } from './AddOnCall';
 import './SpoonacularRequests.css';
 
+/**
+ * Defines the default headers for these functions to work with `json-server`
+ */
+const headers = new Headers();
+headers.append("Content-Type", "application/json");
+
+
+
 export const fetchData = async(addRecipe: (title:string, id:number, image: string)=> void, ingredientList: any) => {
   const ingredientSearch = (ingredients: any) => {
 
@@ -18,6 +26,21 @@ export const fetchData = async(addRecipe: (title:string, id:number, image: strin
   const call = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=e0b11b97f0b64f5982330cabe8d0ec9b&ingredients=${ingredientSearch(ingredientList)}`);
 
   const response = await call.json();
+  console.log(response)
 
   onCallHandler(response, addRecipe, 5);
 }
+
+export const readRecipe = async(recipeId: number) => {
+  const url = `https://api.spoonacular.com/recipes/${recipeId.toString()}/information?apiKey=73a82c046a6043fd8fd23732ec9380c7&includeNutrition=false`
+  const options = {
+    headers
+  };
+  
+  const recipeCall = await fetch(url, options);
+
+  const recipeResponse = await recipeCall.json();
+  console.log(recipeResponse)
+
+  return recipeResponse
+};
