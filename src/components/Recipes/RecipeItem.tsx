3eb0ed2800b65d7a8364b05e180cf0c2ecb48bf6
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2022-04-25 13:08:26 -0700

    feat(RecipePage): individual recipe pages with global info

--- src/components/Recipes/RecipeItem.tsx
+++ src/components/Recipes/RecipeItem.tsx
@@ -2,15 +2,30 @@ import "./Recipes.css"
 import './RecipeItem.css'
 import Card from '../UI/Card';
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { FoodprintContext } from "../../store/foodprint-context";
+import { readRecipe } from "../../utils/SpoonacularRequests";
 
-const RecipeItem: React.FC<{recipeID: number, text: string, image: string}> = (props) => {
+const RecipeItem: React.FC<{recipeID: number, text: string, image: string, }> = (props) => {
   const navigate = useNavigate();
+  const recipeNavigator = () => navigate(`/RecipePage/${props.recipeID.toString()}`)
+  const foodprintCtx = useContext(FoodprintContext);
+  const setRecipeInfo = foodprintCtx.recipeInfo.setRecipeInfo;
+
+  async function fetchRecipeData () {
+   const data = await readRecipe(props.recipeID.toString())
+
+   setRecipeInfo(data)
+
+   recipeNavigator()
+  }
+  
   return (
-    <li>
+    <li key={props.recipeID}>
       <Card class='recipeCard'>
         <img className="card-header" src={props.image} />
         <h2>{props.text}</h2>
-        <button className="button" onClick={() => navigate(`/RecipePage/${props.recipeID.toString()}`)}>
+        <button className="button" onClick={fetchRecipeData}>
           <i className="fa-fa-chevron-right"></i>Recipe
         </button>
       </Card>
