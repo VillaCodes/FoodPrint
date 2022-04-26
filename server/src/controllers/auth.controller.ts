92208717e50cf1acbbc05858d8c4af6e2f7f8dad
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2022-04-25 18:22:55 -0700

    feat(authenticateUser): server authentication initialized

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,21 @@
+import {Request, Response} from "express";
+import { OAuth2Client } from "google-auth-library";
+
+const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+export const authenticateUser = async (req: Request, res:Response) => {
+    const {token} = req.body;
+
+    const ticket = await googleClient.verifyIdToken({
+        idToken: token,
+        audience: `${process.env.GOOGLE_CLIENT_ID}`
+    });
+
+    const payload = ticket.getPayload();
+
+    console.log(`User ${payload.name} verified`);
+
+    const {sub, name, email, picture} = payload;
+
+    const userId = sub;
+};
