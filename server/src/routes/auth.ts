ea13142bd771f283925b2183bad80e2500b69de2
Christian Guizar <c-guizar@hotmail.com>
2022-05-20 16:53:24 -0700

    feat(validation): addeadded real time validation to registration page

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,13 +2,6 @@ const express = require('express');
 
 import { authenticateUser } from "../controllers/auth.controller";
 
-const router = express.Router();
-
-const authenticateUser = async (req, res) => {
-    const {token} = req.body;
-    res.send('she')
-};
+export const router = express.Router();
 
 router.post("/", authenticateUser);
-
-module.exports = router;
--- server/src/server.ts
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const PORT = process.env.PORT || 5000;
-
-const app = require("./app.ts")
-
-
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
