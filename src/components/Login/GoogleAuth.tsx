b4b6180c1ecdc4990c72f5655618bd3b8361f207
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2022-06-20 23:00:03 -0700

    feat(googleIdentityServices): updated deprecated google login with google identity services methods

--- src/components/Login/GoogleAuth.tsx
+++ src/components/Login/GoogleAuth.tsx
@@ -1,6 +1,9 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 import GoogleLogin from "react-google-login";
 import { FoodprintContext } from '../../store/foodprint-context';
+import { useScript } from "../../utils/hooks/useScript";
+import jwt_decode from "jwt-decode";
+import {googleUser} from "../../models/googleUser";
 
 interface User {
   _id: string;
@@ -10,6 +13,7 @@ interface User {
 }
 
 const GoogleAuth = () => {
+  const googleButtonRef = useRef(null);
   const [user, setUser] = useState<User | null>(null);
   const foodprintCtx = useContext(FoodprintContext);
   const { onLogin } = foodprintCtx.login
@@ -37,14 +41,48 @@ const GoogleAuth = () => {
       return error
     }
   };
+
+
+  const onGoogleSignin = async (user: any) => {
+    const userCred = user.credential;
+    const payload: googleUser = jwt_decode(userCred);
+    payload._id = payload.sub;
+
+    const data = {
+      "googleUser": payload
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+
+    const result = await fetch('http://localhost:4000/Login', options);
+
+    const json = await result.json();
+    onLogin(json.enrolled.email, json.enrolled.name);
+    setUser(json.enrolled);
+  };
+
+  useScript("https://accounts.google.com/gsi/client", () => {
+    window.google.accounts.id.initialize({
+      client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+      callback: onGoogleSignin,
+      auto_select: false,
+    });
+
+    window.google.accounts.id.renderButton(googleButtonRef.current, {
+      size: "medium",
+    });
+  });
+
+
   return (
     <div>
-      {!user && (
-        <GoogleLogin
-          clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
-          onSuccess={onSuccess}
-        />
-      )}
+      {!user && <div ref={googleButtonRef}></div>}
 
       {user && (
         <>
